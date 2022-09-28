6ca00d4ed434f58e0813508318e43c2413b46708
skieer-ssv <sidsv21@gmail.com>
2022-09-28 15:43:27 +0530

    feat: add airport model entity

--- src/main/java/com/flights/bean/Airport.java
+++ src/main/java/com/flights/bean/Airport.java
@@ -1,5 +1,53 @@
 package com.flights.bean;
 
-public class Airport {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class Airport {
+	
+	@Id
+	@GeneratedValue
+	private int airportId;
+	private String airportCode; // BOM, DEL, 
+	private String airportName;
+	private String airportLocation;
+	public Airport() {
+		
+	}
+	public Airport(String airportCode, String airportName, String airportLocation) {
+		super();
+		this.airportCode = airportCode;
+		this.airportName = airportName;
+		this.airportLocation = airportLocation;
+	}
+	public String getAirportCode() {
+		return airportCode;
+	}
+	public void setAirportCode(String airportCode) {
+		this.airportCode = airportCode;
+	}
+	public String getAirportName() {
+		return airportName;
+	}
+	public void setAirportName(String airportName) {
+		this.airportName = airportName;
+	}
+	public String getAirportLocation() {
+		return airportLocation;
+	}
+	public void setAirportLocation(String airportLocation) {
+		this.airportLocation = airportLocation;
+	}
+	@Override
+	public String toString() {
+		return "Airport [airportId=" + airportId + ", airportCode=" + airportCode + ", airportName=" + airportName
+				+ ", airportLocation=" + airportLocation + "]";
+	}
+	
+	
+	
+	
+	
 }
