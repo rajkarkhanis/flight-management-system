60bcca07a133f2a33b7b59b8a3143aa2882f4553
sadnaik <sadnaik@INLEN8520055869.corp.capgemini.com>
2022-09-28 16:10:01 +0530

    feat: add user model entity

--- src/main/java/com/flights/bean/User.java
+++ src/main/java/com/flights/bean/User.java
@@ -1,5 +1,77 @@
 package com.flights.bean;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	
+	@Id
+	@GeneratedValue
+	private int userId;
+	private String userType;
+	private String userName;
+	private String userPassword;
+	private String userPhone;
+	private String userEmail;
+	
+	public User(int userId, String userType, String userName, String userPassword, String userPhone, String userEmail) {
+		super();
+		this.userId = userId;
+		this.userType = userType;
+		this.userName = userName;
+		this.userPassword = userPassword;
+		this.userPhone = userPhone;
+		this.userEmail = userEmail;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String getUserPhone() {
+		return userPhone;
+	}
+
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
 
 }
