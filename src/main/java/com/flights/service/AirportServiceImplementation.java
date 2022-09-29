37e48b22d78ac157e75a762e91efdad1cf406798
skieer-ssv <sidsv21@gmail.com>
2022-09-29 15:13:28 +0530

    fix: Autowired AirportDao in Service

--- src/main/java/com/flights/service/AirportServiceImplementation.java
+++ src/main/java/com/flights/service/AirportServiceImplementation.java
@@ -2,11 +2,16 @@ package com.flights.service;
 
 import com.flights.bean.Airport;
 import com.flights.dao.AirportDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class AirportServiceImplementation implements AirportService{
 
+    @Autowired
     AirportDao airportDao;
 
     @Override
