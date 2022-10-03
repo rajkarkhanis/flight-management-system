4b64c71ca4fbe3187ea5951e84bfcda21614b39a
JUHI1511 <vishakhajuhi11@gmail.com>
2022-10-03 14:13:23 +0530

    feat:Implemented Swagger and Tests

--- src/main/java/com/flights/FlightsApplication.java
+++ src/main/java/com/flights/FlightsApplication.java
@@ -2,10 +2,24 @@ package com.flights;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FlightsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FlightsApplication.class, args);
+
 	}
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.flights")).build();
+	}
+	//http://localhost:8080/swagger-ui/index.html
 }
